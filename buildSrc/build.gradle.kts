4c203d1846dfd48d833172798cf20ac52ea82b03
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2020-09-04 10:17:57 +0200

    Add Easylauncher

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    google()
+    jcenter()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:4.0.1")
+    implementation("com.project.starter:easylauncher:3.2.1")
+}
